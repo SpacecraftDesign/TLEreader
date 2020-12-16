85d57b97bd81aa9ef258110c2c808e8106b41ea8
Martin Kamme <martinkamme@gmail.com>
2020-12-16 19:45:09 +0100

    Updating TLEreader to match ClientTest

--- TLEreader.ino
+++ TLEreader.ino
@@ -1,25 +1,22 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
+
 #include <ESP8266HTTPClient.h>
-#include <WiFiClientSecure.h>
+
 
 const char* ssid = "SFR_B228";
 const char* password = "9kpm59hjht6cay63arwa";
 
-WiFiClientSecure httpsClient;
+WiFiClient Client;
 #define host "www.celestrak.com"
-#define testHost "www.tindie.com"
 
-const char fingerprint[] PROGMEM = "3a ce 54 bc 41 42 e7 4f 54 60 c9 3c a9 e7 a8 71 1b 5a 5e 1c";
-const int httpsPort = 443;
-
-ESP8266WiFiMulti WiFiMulti;
+//const char fingerprint[] PROGMEM = "3a ce 54 bc 41 42 e7 4f 54 60 c9 3c a9 e7 a8 71 1b 5a 5e 1c";
+const int httpsPort = 80; //443 for HTTPS
 
 void setup() {
   Serial.begin(115200);
 
   WiFi.begin(ssid,password);
-  Serial.flush();
+  Serial.flush();  // not sure what this command does
   Serial.println("Connecting to wifi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -30,15 +27,15 @@ void setup() {
 }
 
 void makeHTTPSrequest(){
-  
+
   if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Connected to WiFi"); 
-      //httpsClient.setFingerprint(fingerprint);     
+      Serial.println("Connected to WiFi");
+      //httpsClient.setFingerprint(fingerprint);
       //httpsClient.setTimeout(1000);
       delay(1000);
-      int val = httpsClient.connect(testHost, 443);
+      int val = httpsClient.connect(testHost, 80);
       Serial.println(val);
-      if (httpsClient.connect(host, 443) > 0){
+      if (httpsClient.connect(host, 80) > 0){
         Serial.println(F("Ready for request"));
         return;
       }
@@ -48,7 +45,7 @@ void makeHTTPSrequest(){
         httpsClient.readBytes(&c,1);
         Serial.println(c);
       }
-      
+
   }
 }
 
