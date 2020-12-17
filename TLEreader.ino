c2228be12f036fa2b744fddc55156b66a7889fa4
Martin Kamme <martinkamme@gmail.com>
2020-12-17 11:55:28 +0100

    Changed baud rate to 9600 Serial Monitor works nicely Make sure language in arduino is set to English

--- TLEreader.ino
+++ TLEreader.ino
@@ -1,22 +1,20 @@
 #include <ESP8266WiFi.h>
-
 #include <ESP8266HTTPClient.h>
 
 
 const char* ssid = "SFR_B228";
 const char* password = "9kpm59hjht6cay63arwa";
+char servername[]="celestrak.com";
 
-WiFiClient Client;
-#define host "www.celestrak.com"
+WiFiClient client;
 
 //const char fingerprint[] PROGMEM = "3a ce 54 bc 41 42 e7 4f 54 60 c9 3c a9 e7 a8 71 1b 5a 5e 1c";
 const int httpsPort = 80; //443 for HTTPS
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);   // changing baud rate for serial monitor
 
   WiFi.begin(ssid,password);
-  Serial.flush();  // not sure what this command does
   Serial.println("Connecting to wifi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -27,26 +25,54 @@ void setup() {
 }
 
 void makeHTTPSrequest(){
-
-  if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Connected to WiFi");
-      //httpsClient.setFingerprint(fingerprint);
-      //httpsClient.setTimeout(1000);
-      delay(1000);
-      int val = httpsClient.connect(testHost, 80);
-      Serial.println(val);
-      if (httpsClient.connect(host, 80) > 0){
-        Serial.println(F("Ready for request"));
-        return;
-      }
-      char c = 0;
-      while(httpsClient.available()){
-        Serial.println("The Client is available");
-        httpsClient.readBytes(&c,1);
-        Serial.println(c);
-      }
-
+  // Port number for HTTPS connection: 443
+  // if you get a connection, report back via serial:
+  if (client.connect(servername, 80)) {
+  Serial.println("connected to server");
+  Serial.println();
+  Serial.print("TLE for: ");
+  // Make HTTP request:
+  client.println("GET /NORAD/elements/iridium-33-debris.txt HTTP/1.0");     // rest of url, i.e extension following celestrak.com
+  client.println();                                                         // necessary to call on rest of server information
   }
+
+char c;
+int lineCounter=0;
+while (!client.available()){
+// while loop runs while waiting for server availability
+}
+
+// Skip HTTP headers
+char endOfHeaders[] = "\r\n\r\n";
+if (!client.find(endOfHeaders))
+{
+  Serial.println(F("Invalid response"));
+  return;
+}
+
+// if there are incoming bytes available from the server, read them and print them:
+while (client.available()) {
+  c = client.read();
+  Serial.print(c);
+
+  if (c == '\n'){
+    lineCounter = lineCounter+1;
+    //Serial.print("Line count: ");
+    //Serial.print(lineCounter);
+  }
+
+  if (lineCounter==3){
+    client.stop();
+    break;
+  }
+}
+
+// if the server becomes disconnected, stop the client:
+if (!client.connected()) {
+  Serial.println();
+  Serial.println("disconnecting from server");
+  client.stop();
+}
 }
 
 void loop() {
